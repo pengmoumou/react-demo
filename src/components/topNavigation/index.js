9e4cad2593620177ab0d9c68fe7456a077e44cfc
pengmoumou <1813564200@qq.com>
2019-03-20 19:16:34 +0800

    created

---
+++ src/components/topNavigation/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class TopNavigation extends Component {
+  render() {
+    return (<div className="top-navigation"><div>this is my first react demo</div>
+    </div>
+    )
+  }
+}
+
+export default TopNavigation;
